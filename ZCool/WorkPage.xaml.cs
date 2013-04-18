9627752fd09d879384aa1f7738b4ef349368f477
Wei Liang <dothegod@gmail.com>
2013-04-18 22:33:14 +0800

    添加Work页面

---
+++ ZCool/WorkPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using MyLib.HttpLib;
+
+namespace ZCool
+{
+    public partial class WorkPage : PhoneApplicationPage
+    {
+        public WorkPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PhoneApplicationPage_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            if (NavigationContext.QueryString.Count > 0)
+            {
+                string webUri = NavigationContext.QueryString["Target"];
+                DownloadHelper dl = new DownloadHelper();
+                dl.DownloadCallbackEvent += OnLoadReviewsComplete;
+                dl.HttpWebRequestDownloadGet(webUri);
+            }
+            else
+            {
+                MessageBox.Show("抱歉，页面参数错误！");
+            }
+            
+        }
+
+        private void OnLoadReviewsComplete(object sender, DownloadEventArgs e)
+        {
+            string Content = e._DownloadString;
+        }
+
+    }
+}
---
+++ ZCool/WorkParser.cs
@@ -0,0 +1,22 @@
+﻿using MyLib.HttpLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZCool
+{
+    class WorkParser
+    {
+        public List<string> Parser(string Content)
+        {
+            if (string.IsNullOrEmpty(Content))
+            {
+                return null;
+            }
+            HtmlParser Parser = new HtmlParser(Content);
+
+
+        }
+    }
+}
